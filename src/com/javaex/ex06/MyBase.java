d0a1200ff16b01bf106875c64b34728cfc772868
'SeoWoongki' <'tjndndrl0922@naver.com'>
2020-12-02 18:18:53 +0900

    Practice06

--- src/com/javaex/ex06/MyBase.java
+++ src/com/javaex/ex06/MyBase.java
@@ -3,29 +3,36 @@ package com.javaex.ex06;
 public class MyBase extends Base {
 
 	// 코드작성
+
+	/*
+	 * public void day() { System.out.println("낮에는 열심히 수업듣자"); }
+	 * 
+	 * public void night() { System.out.println("밤에는 숙면"); }
+	 * 
+	 * 
+	 * public void service(String state) {
+	 * 
+	 * switch (state) { case "낮": day(); break;
+	 * 
+	 * case "밤": night(); break;
+	 * 
+	 * default: System.out.println("오후도 낮과 마찬가지로 공부해야합니다."); break; } }
+	 */
+
+	public void service(String state) {
+		if (state.equals("오후")) {
+			afternoon();
+		} else {
+			super.service(state);
+		}
+	}
+
 	public void day() {
 		System.out.println("낮에는 열심히 수업듣자");
 	}
 
-	public void night() {
-		System.out.println("밤에는 숙면");
-	}
-
-	public void service(String state) {
-
-		switch (state) {
-		case "낮":
-			day();
-			break;
-
-		case "밤":
-			night();
-			break;
-
-		default:
-			System.out.println("오후도 낮과 마찬가지로 공부해야합니다.");
-			break;
-		}
+	public void afternoon() {
+		System.out.println("오후도 낮과 마찬가지로 공부해야 합니다.");
 	}
 
 }
